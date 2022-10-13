45adf62bdcb65f388b8bfda1d16bf1d8d56675e7
MD Iasir Arafat Riaz <iasir.arafat.riaz@gmail.com>
2022-10-13 16:46:51 +0600

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Text from './components/text';
+// import Text from "./components/text"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>fist react native project</Text>
+      <Text>component</Text>
       <StatusBar style="auto" />
     </View>
   );
